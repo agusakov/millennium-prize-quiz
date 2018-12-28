2d67ed04ffb47ec88785b728c34d35bb494524f3
Alena Gusakov <agusakov@udel.edu>
2018-12-27 20:25:05 -0500

    Updates

---
+++ precache-manifest.38870d95a69f982757187de32bce6381.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f84080265e47d145dc42",
+    "url": "/millennium-prize-quiz/static/js/runtime~main.f8408026.js"
+  },
+  {
+    "revision": "7c958a014738301bb8d5",
+    "url": "/millennium-prize-quiz/static/js/main.7c958a01.chunk.js"
+  },
+  {
+    "revision": "78ca7d800146092727db",
+    "url": "/millennium-prize-quiz/static/js/1.78ca7d80.chunk.js"
+  },
+  {
+    "revision": "7c958a014738301bb8d5",
+    "url": "/millennium-prize-quiz/static/css/main.5ce87491.chunk.css"
+  },
+  {
+    "revision": "4e68ac6c7c58f8e4860370428f9ae8b6",
+    "url": "/millennium-prize-quiz/index.html"
+  }
+];
